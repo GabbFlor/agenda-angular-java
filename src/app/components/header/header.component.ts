4b74dedfc8f09ad7beeb28a394dba5a355f12bce
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2025-03-20 16:58:39 -0300

    feat
    - Criando um link para rota padrão "/"

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
